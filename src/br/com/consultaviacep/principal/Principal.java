a51be01fe6d125a0424e0e9b5afd22bb87595337
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-07 22:04:04 -0300

    Adicional objeto da classe ConsultaHttp e invocar o método fazerRequisicao

--- src/br/com/consultaviacep/principal/Principal.java
+++ src/br/com/consultaviacep/principal/Principal.java
@@ -1,7 +1,15 @@
 package br.com.consultaviacep.principal;
 
+import br.com.consultaviacep.modelos.ConsultaHTTP;
+import java.io.IOException;
+
+
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("teste");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ConsultaHTTP consultaHTTP = new ConsultaHTTP("86806632");
+        System.out.println(consultaHTTP.fazerRequisicao());
+
+
     }
 }
