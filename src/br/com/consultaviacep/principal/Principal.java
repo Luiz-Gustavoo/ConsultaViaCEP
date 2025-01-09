734ae2afb6358408b859d1eec84b62cbc864bfdf
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-08 21:34:22 -0300

    Adicionar instância da classe EnderecoViaCep e da classe Endereco

--- src/br/com/consultaviacep/principal/Principal.java
+++ src/br/com/consultaviacep/principal/Principal.java
@@ -1,6 +1,9 @@
 package br.com.consultaviacep.principal;
 
 import br.com.consultaviacep.modelos.ConsultaHTTP;
+import br.com.consultaviacep.modelos.Endereco;
+import br.com.consultaviacep.modelos.EnderecoViaCEP;
+
 import java.io.IOException;
 
 
@@ -8,7 +11,10 @@ public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         ConsultaHTTP consultaHTTP = new ConsultaHTTP("86806632");
-        System.out.println(consultaHTTP.fazerRequisicao());
+        EnderecoViaCEP enderecoViaCEP = consultaHTTP.fazerRequisicao();
+
+        Endereco endereco = new Endereco(enderecoViaCEP);
+        System.out.println(endereco);
 
 
     }
