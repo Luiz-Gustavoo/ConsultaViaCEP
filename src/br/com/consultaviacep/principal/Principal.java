df20c2595b93fe8bec2493c5a164c5f466f27607
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-10 20:41:20 -0300

    Usar a classe LerCEP para ler e validar o CEP

--- src/br/com/consultaviacep/principal/Principal.java
+++ src/br/com/consultaviacep/principal/Principal.java
@@ -1,17 +1,13 @@
 package br.com.consultaviacep.principal;
-
-import br.com.consultaviacep.excecoes.CEPinvalidoExcepton;
 import br.com.consultaviacep.modelos.*;
-
 import java.io.IOException;
 
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        String busca = "86806632";
-
-        ResultadoValidaCEP resultadoValidaCEP = lerCEP(busca);
+        String busca = "1";
+        ResultadoValidaCEP resultadoValidaCEP = LerCEP.lerCEP(busca);
         if (!resultadoValidaCEP.isResultadoValidaCEP()) {
             System.out.println("Não foi possível validar esse CEP");
         } else {
@@ -24,18 +20,6 @@ public class Principal {
 
         }
     }
-        public static ResultadoValidaCEP lerCEP(String cep) {
-            ValidaCEP validaCEP = new ValidaCEP();
-        try {
-            if(!validaCEP.validaCEP(cep)) {
-                throw new CEPinvalidoExcepton(validaCEP.getMensagemValidacao());
-            }
-        } catch (CEPinvalidoExcepton e) {
-            System.out.println("Erro de validação no CEP: " + e.getMensagem());
-            return new ResultadoValidaCEP(cep, false);
-        }
-        return new ResultadoValidaCEP(cep, true);
-        }
     }
 
 
