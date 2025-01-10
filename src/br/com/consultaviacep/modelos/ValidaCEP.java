0c9b06b7b33fffb8cfa0dacc86909988df4d1f7e
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-09 21:20:02 -0300

    Adicionar atributo mensagemValidacao, adiconar getter de mensagemValidacao, alterar o método validaCEP

--- src/br/com/consultaviacep/modelos/ValidaCEP.java
+++ src/br/com/consultaviacep/modelos/ValidaCEP.java
@@ -1,9 +1,15 @@
 package br.com.consultaviacep.modelos;
 
 public class ValidaCEP {
+    private String mensagemValidacao;
+
+    public String getMensagemValidacao() {
+        return mensagemValidacao;
+    }
 
     public boolean validaCEP(String cep) {
         if (cep.length() != 8) {
+            mensagemValidacao = "O CEP não possui 8 dígitos";
             return false;
         } else {
             return true;
