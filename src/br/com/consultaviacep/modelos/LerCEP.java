6f2952d9846081a918579e3515c814459b6a94af
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-10 20:40:40 -0300

    Adicionar classe LerCEP

---
+++ src/br/com/consultaviacep/modelos/LerCEP.java
@@ -0,0 +1,19 @@
+package br.com.consultaviacep.modelos;
+
+import br.com.consultaviacep.excecoes.CEPinvalidoExcepton;
+
+public class LerCEP {
+
+    public static ResultadoValidaCEP lerCEP(String cep) {
+        ValidaCEP validaCEP = new ValidaCEP();
+        try {
+            if(!validaCEP.validaCEP(cep)) {
+                throw new CEPinvalidoExcepton(validaCEP.getMensagemValidacao());
+            }
+        } catch (CEPinvalidoExcepton e) {
+            System.out.println("Erro de validação no CEP: " + e.getMensagem());
+            return new ResultadoValidaCEP(cep, false);
+        }
+        return new ResultadoValidaCEP(cep, true);
+    }
+}
