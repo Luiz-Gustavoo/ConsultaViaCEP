71b00e74299f018b3c99d6dfc55142b48cecc7c3
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-09 21:31:26 -0300

    Alterar exceção CEPinvalidoExcepton

--- src/br/com/consultaviacep/excecoes/CEPinvalidoExcepton.java
+++ src/br/com/consultaviacep/excecoes/CEPinvalidoExcepton.java
@@ -1,19 +1,14 @@
 package br.com.consultaviacep.excecoes;
 
 public class CEPinvalidoExcepton extends RuntimeException {
-    private String cep;
-    private boolean resultadoValidacao;
 
-  public String getCep() {
-    return cep;
+    private String mensagem;
+
+  public String getMensagem() {
+    return mensagem;
   }
 
-  public boolean isResultadoValidacao() {
-    return resultadoValidacao;
-  }
-
-  public CEPinvalidoExcepton(String cep, boolean resultadoValidacao) {
-    this.cep = cep;
-    this.resultadoValidacao = resultadoValidacao;
+  public CEPinvalidoExcepton(String mensagem) {
+    this.mensagem = mensagem;
   }
 }
