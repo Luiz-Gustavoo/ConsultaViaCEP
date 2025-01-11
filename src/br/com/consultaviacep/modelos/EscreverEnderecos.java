7ae1e5acea03dac270125d83146a6c8061a7725f
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-11 16:30:57 -0300

    Adicionar atributo gson, adicionar try-catch no método escreverEnderecos

--- src/br/com/consultaviacep/modelos/EscreverEnderecos.java
+++ src/br/com/consultaviacep/modelos/EscreverEnderecos.java
@@ -1,18 +1,30 @@
 package br.com.consultaviacep.modelos;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class EscreverEnderecos {
     FileWriter escrita = new FileWriter("D:\\Windows Sistema\\Documentos\\Enderecos.txt");
+    Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
 
     public EscreverEnderecos() throws IOException {
     }
 
     public String escreverEnderecos(List<Endereco> listaEnderecos) throws IOException {
-        escrita.write(listaEnderecos.toString());
-        escrita.close();
-        return "Lista salva no arquivo";
+        try {
+            escrita.write(gson.toJson(listaEnderecos));
+            escrita.close();
+            return "Lista salva no arquivo";
+        } catch(FileNotFoundException e) {
+            System.out.println("Mão foi possível localizar o arquivo. Verifique se nome do mesmo está correto ou se existe");
+            return "Não foi possível salvar os dados";
+        }
     }
 }
