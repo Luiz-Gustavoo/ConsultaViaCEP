e79c7bad75dd4a002e16a2616c3cbc887914a227
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-11 15:31:15 -0300

    Adicionar lista de endereços, Iterar sobre uma lista de endereços

--- src/br/com/consultaviacep/principal/Principal.java
+++ src/br/com/consultaviacep/principal/Principal.java
@@ -1,12 +1,15 @@
 package br.com.consultaviacep.principal;
 import br.com.consultaviacep.modelos.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        List<Endereco> listaEndereco = new ArrayList<>();
         String busca = "";
 
         while (!busca.equalsIgnoreCase("sair")) {
@@ -26,9 +29,15 @@ public class Principal {
 
                 Endereco endereco = new Endereco(enderecoViaCEP);
                 System.out.println(endereco);
-
+                listaEndereco.add(endereco);
             }
+
         }
+        System.out.println("Lista de endereços: ");
+        for(Endereco endereco: listaEndereco) {
+            System.out.println(endereco);
+        }
+        System.out.println("Finalizando a aplicação");
     }
 }
 
